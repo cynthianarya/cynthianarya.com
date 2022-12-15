62458318697a1fb990e648fe89006388069d0236
Cynthia Naranjo Yagual <cynthianaranjoyagual@gmail.com>
2022-12-15 16:27:11 -0500

    test3

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '../data/logo.png'
+import Logo from '@data/logo.png'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -16,7 +16,6 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <img src={Logo} alt="Logo" />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
