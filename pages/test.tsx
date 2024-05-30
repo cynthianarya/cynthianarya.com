fc3f7396071babeb4b72021b8ca57fe3954b69f4
Cristhian García Vélez <escipion25@gmail.com>
2024-05-29 20:02:17 -0500

    Update pages

---
+++ pages/test.tsx
@@ -0,0 +1,15 @@
+# Getting Started on Mobile
+
+👋 Welcome to Notion!
+
+Here are the basics:
+
+- [ ]  Tap anywhere and start typing
+- [ ]  Tap the + above your keyboard to add content — headers, sub pages, etc.
+    
+    [Example sub page](https://www.notion.so/Example-sub-page-813a1c572fc44fd48c0094560a19d81a?pvs=21)
+    
+- [ ]  Highlight text and use the bar above your keyboard to format
+- [ ]  Tap and hold this line, then drag
+- [ ]  Tap the home tab button at the bottom left to see your pages
+- [ ]  Tap anywhere and select ✨ in the bar above your keyboard to check out **Notion AI**
